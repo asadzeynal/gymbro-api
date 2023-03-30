9c80d6f4e44327091da5c8c54de7c7e5cd87d9fe
asadzeynal <asadzeynal9@gmail.com>
2023-03-31 03:40:58 +0400

    fix(db): updated the initial migration replaced dbml with valid sql

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,66 +1,82 @@
-Table exercises {
-  id uuid [pk]
-  name varchar [not null]
-  description varchar
-}
+CREATE TABLE "exercises" (
+  "id" uuid PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "description" varchar
+);
 
-Table users {
-  id uuid [primary key]
-  email varchar(255) [not null, unique]
-  password varchar(255) [not null]
-  created_at timestamp [not null, default: `now()`]
-  updated_at timestamp [not null, default: `now()`]
-}
+CREATE TABLE "users" (
+  "id" uuid PRIMARY KEY,
+  "email" varchar(255) UNIQUE NOT NULL,
+  "password" varchar(255) NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
+);
 
-Table templates {
-  id uuid [primary key]
-  name varchar(255) [not null]
-  user_id uuid [not null, ref: > users.id]
-  created_at timestamp [not null, default: `now()`]
-  updated_at timestamp [not null, default: `now()`]
-}
+CREATE TABLE "templates" (
+  "id" uuid PRIMARY KEY,
+  "name" varchar(255) NOT NULL,
+  "user_id" uuid NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
+);
 
-Table template_exercises {
-  id uuid [primary key]
-  template_id uuid [not null, ref: > templates.id]
-  exercise_id varchar(255) [not null, ref: > exercises.id]
-  display_order int [not null, default: 0]
-  created_at timestamp [not null, default: `now()`]
-  updated_at timestamp [not null, default: `now()`]
-}
+CREATE TABLE "template_exercises" (
+  "id" uuid PRIMARY KEY,
+  "template_id" uuid NOT NULL,
+  "exercise_id" varchar(255) NOT NULL,
+  "display_order" int NOT NULL DEFAULT 0,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
+);
 
-Table template_sets {
-  id uuid [primary key]
-  template_exercise_id uuid [not null, ref: > template_exercises.id]
-  reps int [not null, default: 0]
-  weight float [not null, default: 0.0]
-  created_at timestamp [not null, default: `now()`]
-  updated_at timestamp [not null, default: `now()`]
-}
+CREATE TABLE "template_sets" (
+  "id" uuid PRIMARY KEY,
+  "template_exercise_id" uuid NOT NULL,
+  "reps" int NOT NULL DEFAULT 0,
+  "weight" float NOT NULL DEFAULT 0,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
+);
 
-Table workouts {
-  id uuid [primary key]
-  user_id uuid [not null, ref: > users.id]
-  name varchar(255) [not null]
-  date timestamp [not null, default: `now()`]
-  created_at timestamp [not null, default: `now()`]
-  updated_at timestamp [not null, default: `now()`]
-}
+CREATE TABLE "workouts" (
+  "id" uuid PRIMARY KEY,
+  "user_id" uuid NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "date" timestamp NOT NULL DEFAULT (now()),
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
+);
 
-Table workout_exercises {
-  id uuid [primary key]
-  workout_id uuid [not null, ref: > workouts.id]
-  exercise_id varchar(255) [not null, ref: > exercises.id]
-  display_order int [not null, default: 0]
-  created_at timestamp [not null, default: `now()`]
-  updated_at timestamp [not null, default: `now()`]
-}
+CREATE TABLE "workout_exercises" (
+  "id" uuid PRIMARY KEY,
+  "workout_id" uuid NOT NULL,
+  "exercise_id" varchar(255) NOT NULL,
+  "display_order" int NOT NULL DEFAULT 0,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
+);
 
-Table workout_sets {
-  id uuid [primary key]
-  workout_exercise_id uuid [not null, ref: > workout_exercises.id]
-  reps int [not null, default: 0]
-  weight float [not null, default: 0.0]
-  created_at timestamp [not null, default: `now()`]
-  updated_at timestamp [not null, default: `now()`]
-}
+CREATE TABLE "workout_sets" (
+  "id" uuid PRIMARY KEY,
+  "workout_exercise_id" uuid NOT NULL,
+  "reps" int NOT NULL DEFAULT 0,
+  "weight" float NOT NULL DEFAULT 0,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "templates" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "template_exercises" ADD FOREIGN KEY ("template_id") REFERENCES "templates" ("id");
+
+ALTER TABLE "template_exercises" ADD FOREIGN KEY ("exercise_id") REFERENCES "exercises" ("id");
+
+ALTER TABLE "template_sets" ADD FOREIGN KEY ("template_exercise_id") REFERENCES "template_exercises" ("id");
+
+ALTER TABLE "workouts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "workout_exercises" ADD FOREIGN KEY ("workout_id") REFERENCES "workouts" ("id");
+
+ALTER TABLE "workout_exercises" ADD FOREIGN KEY ("exercise_id") REFERENCES "exercises" ("id");
+
+ALTER TABLE "workout_sets" ADD FOREIGN KEY ("workout_exercise_id") REFERENCES "workout_exercises" ("id");

002e63b769573aebed62c9c20807432e7e3b81e8
asadzeynal <asadzeynal9@gmail.com>
2023-04-02 06:25:09 +0400

    feat(domain): Exercise domain

---
+++ domain/exercise.go
@@ -0,0 +1,22 @@
+package domain
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+// Exercise is representing the Exercise data struct
+type Exercise struct {
+	ID          uuid.UUID
+	Name        string
+	Description string
+}
+
+// ExerciseRepository represents the exercise's repository contract
+type ExerciseRepository interface {
+	Store(ctx context.Context, exercise *Exercise) (uuid.UUID, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+	Fetch(ctx context.Context) ([]Exercise, error)
+	GetById(ctx context.Context, id uuid.UUID) (Exercise, error)
+}

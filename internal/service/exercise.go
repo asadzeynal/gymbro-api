208b9c47945c8e95cf3b25b85adf6222653225cf
asadzeynal <asadzeynal9@gmail.com>
2023-04-04 22:16:32 +0400

    feat(service): First method in Exercise service
    - Method that fetches all exercises and returns them grouped bt initial

--- internal/service/exercise.go
+++ internal/service/exercise.go
@@ -1,16 +1,39 @@
 package service
 
-import "github.com/asadzeynal/gymbro-api/internal/domain"
+import (
+	"context"
+	"time"
+
+	"github.com/asadzeynal/gymbro-api/internal/domain"
+)
 
 type exerciseService struct {
-	exerciseRepo domain.ExerciseRepository
+	exerciseRepo   domain.ExerciseRepository
+	contextTimeout time.Duration
 }
 
-func NewExerciseService(er domain.ExerciseRepository) *exerciseService {
-	return &exerciseService{er}
+func NewExerciseService(er domain.ExerciseRepository, timeout time.Duration) *exerciseService {
+	return &exerciseService{er, timeout}
 }
 
 // Returns Exercises grouped by first letter
-func FetchGroupedByInitial() {
+func (es *exerciseService) FetchGroupedByInitial(c context.Context) (map[string][]domain.Exercise, error) {
+	ctx, cancel := context.WithTimeout(c, es.contextTimeout)
+	defer cancel()
 
+	res, err := es.exerciseRepo.Fetch(ctx)
+	if err != nil {
+		return nil, err
+	}
+	grouped := groupExerciseByInitial(res)
+	return grouped, nil
+}
+
+func groupExerciseByInitial(exs []domain.Exercise) map[string][]domain.Exercise {
+	grouped := make(map[string][]domain.Exercise)
+	for i := range exs {
+		initial := exs[i].Name[:1]
+		grouped[initial] = append(grouped[initial], exs[i])
+	}
+	return grouped
 }
